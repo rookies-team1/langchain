24b494bc919337c381d4e71aa01c71d4955b788c
nahye <knh8783@gmail.com>
2025-07-04 11:26:20 +0900

    feat: add news_data and connect summarizer to FastAPI

--- llm-service/main.py
+++ llm-service/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from summarizer import summarize_news
 
 app = FastAPI(title="AI Agent API")
 
 @app.get("/")
 def read_root():
     return {"message": "LLM Service is running."}
+
+# summarizer POST 요청 처리
+
+class News(BaseModel):
+    title: str
+    content: str
+
+@app.post("/summarize")
+async def summarize(news: News):
+    try:
+        summary = summarize_news(news.dict())
+        return {"summary": summary}
+    except Exception as e:
+        return {"error": str(e)}
