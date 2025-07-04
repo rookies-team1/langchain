3b4eb93c88dcdcc0ae1202ab2ccfbbdc4b9a02ae
Kwon Soonmin <sogno6037@gmail.com>
2025-07-04 13:33:21 +0900

    action test

--- llm-service/main.py
+++ llm-service/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI(title="AI Agent API")
 
+# 환경 변수에서 API 키 로드
+google_api_key = os.getenv("GOOGLE_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+ 
 @app.get("/")
 def read_root():
-    return {"message": "LLM Service is running."}
+    if google_api_key:
+        return {"message": "LLM Service is running.", "api_key_status": "loaded", "key" : google_api_key[:2]}
+    else:
+        return {"message": "LLM Service is running.", "api_key_status": "not loaded"}
