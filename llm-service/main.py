10fa10891e11053a2a0cd8c82a31460554c87117
nahye <knh8783@gmail.com>
2025-07-04 15:40:27 +0900

    add summarizer post annotation

--- llm-service/main.py
+++ llm-service/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import os
+from pydantic import BaseModel
+from summarizer import summarize_news
 
 app = FastAPI(title="AI Agent API")
 
@@ -13,3 +15,18 @@ def read_root():
         return {"message": "LLM Service is running.", "api_key_status": "loaded", "key" : google_api_key[:2]}
     else:
         return {"message": "LLM Service is running.", "api_key_status": "not loaded"}
+    
+
+    # summarizer POST 요청 처리
+
+class News(BaseModel):
+    title: str
+    content: str
+
+@app.post("/summarize")
+async def summarize(news: News):
+    try:
+        summary = summarize_news(news.dict())
+        return {"summary": summary}
+    except Exception as e:
+        return {"error": str(e)}
